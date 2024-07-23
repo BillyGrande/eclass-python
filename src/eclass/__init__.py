d20655e82dd8ac7a47a88fe9bdba7235b11af676
Bill <billxs@hotmail.com>
2024-07-23 23:46:59 +0300

    Write some code to initialize db and create a user table

--- src/eclass/__init__.py
+++ src/eclass/__init__.py
@@ -2,3 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 import eclass.views
+
+from eclass.db.db import db_session
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
