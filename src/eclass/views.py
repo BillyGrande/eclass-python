130a150c2aed0fe239024322039cd7a943c6cf90
XYKIS Vasileios <vxykis@netcompany.com>
2024-07-24 16:56:41 +0300

    Refactor quiz view, add code for session but it is not implemented yet

--- src/eclass/views.py
+++ src/eclass/views.py
@@ -1,8 +1,10 @@
 from eclass import app
-from flask import render_template
+from flask import render_template, session, request,redirect,url_for
 from eclass.db.db import db_session
 from eclass.db.models import Question
 from sqlalchemy import select
+from flask import session
+fr
 
 @app.route("/")
 def hello_world():
@@ -21,12 +23,30 @@ def test_welcome():
     return render_template('tests_welcome.html')
 
 @app.route("/tests/<chapter>")
-def quiz_index(chapter=0):
-    return render_template('quiz_index.html', chapter=chapter)
-
 @app.route("/tests/<chapter>/<id>")
-def quiz_quetion(chapter=None, id=0):
-    statement = select(Question).filter_by(chapter=id)
-    user_obj = db_session.scalars(statement).all()
-    db_session.query()
-    return render_template('quiz_questions.html', question=user_obj[0])
+def quiz(chapter=None, id=None):
+    if id == None:
+        return render_template('quiz_index.html', chapter=chapter)
+    else:
+        statement = select(Question).filter_by(chapter=id)
+        user_obj = db_session.scalars(statement).all()
+        db_session.query()
+        return render_template('quiz_questions.html', question=user_obj[0])
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+        '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
