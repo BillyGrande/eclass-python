484ac8a264102137fe7e3ad37e4053a8c3bc9b30
Bill <billxs@hotmail.com>
2024-08-01 01:04:09 +0300

    Database path shoulb be fixed

--- src/eclass/db/db.py
+++ src/eclass/db/db.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 import os
 
-file_path = os.path.abspath(os.getcwd()) +"eclass.db"
+#file_path = os.path.abspath(os.getcwd()) +"eclass.db"
+file_path = os.path.join(os.path.dirname(__file__), 'eclass.db')
+print(file_path)
 
 #engine = create_engine('sqlite:///db/eclass.db')
-engine = create_engine('sqlite:///'+file_path)
+engine = create_engine('sqlite:///'+ file_path)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 
 Base = declarative_base()
